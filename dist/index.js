699c22d289266b1a1e7d226c53c2382469bbd7b4
muffafa <mm.savar@gmail.com>
2023-01-02 01:17:58 +0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let user = ["mustafa", 123456];
+console.log(user[0].toUpperCase());
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// let myName: string = "Muffafa"
+// myName = myName.toLowerCase()
+// console.log(myName)
+
+// let user = {name: "msutafa", password: "123456"}
+
+// console.log("hello world")
+// console.log(user.name)
+// console.log(user.email)
+
+// let age: number = 5
+// age = 'five'
+// if(age<18)
+// {
+//     age+=10
+// }
+
+// console.log(age)
+
+// let greetings: string = "hello world"
+// greetings = greetings.toUpperCase()
+// console.log(greetings)
+
+let user: [string, number] = ["mustafa", 123456]
+console.log(user[0].toUpperCase())
+
